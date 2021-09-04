dc0de6f73351811ec328b3f4643734e1d59e5c39
Pavol Hejny <me@pavolhejny.com>
2021-09-04 02:05:11 +0200

    Getting app name

---
+++ src/utils/getAppName.ts
@@ -0,0 +1,28 @@
+import { capitalizeFirstLetter } from 'n12';
+
+/**
+ *
+ * @param appPath path to the app executable
+ * @returns human-readable app name
+ */
+export async function getAppName(executablePath: string): Promise<string> {
+    // TODO: Get app name from real exif information not the filename
+
+    const match = executablePath.match(
+        /(?<browserName>[^\\\/\.]*)(?:(?:[^\\\/])*)$/i,
+    );
+
+    if (match) {
+        let appName = match.groups!.browserName!;
+
+        if (appName === 'iexplore') {
+            return 'Internet Explorer';
+        } else if (appName === 'msedge') {
+            return 'Edge';
+        } else {
+            return capitalizeFirstLetter(appName);
+        }
+    }
+
+    throw new Error(`Can not get name of "${executablePath}".`);
+}
