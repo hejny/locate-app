6836eff25dab6df45fc90914583cf11079c82307
Pavol Hejny <me@pavolhejny.com>
2023-12-27 12:51:30 +0100

    Rename `locateVSCode` to `locateVscode`

--- src/test-by-run.ts
+++ src/test-by-run.ts
@@ -1,4 +1,4 @@
-import { locateVSCode } from './main';
+import { locateVscode } from './main';
 
 test();
 
@@ -6,5 +6,5 @@ async function test() {
     // TODO: Return path not name> console.info('Default browser: ', await locateDefaultSystemBrowser());
     // console.info('Firefox: ', await locateFirefox());
     // console.info('Chrome: ', await locateChrome());
-    console.info('VSCode: ', await locateVSCode());
+    console.info('VSCode: ', await locateVscode());
 }
